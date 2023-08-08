ae17ef8c05273442c5aa4b29cdf602ace7687c6d
CzaplaKarolina <karolina.czapla@onet.com.pl>
2023-08-08 22:06:51 +0200

    X

--- src/pages/masterPage.js
+++ src/pages/masterPage.js
@@ -2,4 +2,618 @@
 
 $w.onReady(function () {
 
+    
+
+SKIN_CLASSES = {
+    0: 'akiec, Actinic Keratoses (Solar Keratoses) or intraepithelial Carcinoma (Bowen’s disease)',
+    1: 'bcc, Basal Cell Carcinoma',
+    2: 'bkl, Benign Keratosis',
+    3: 'df, Dermatofibroma',
+    4: 'mel, Melanoma',
+    5: 'nv, Melanocytic Nevi',
+    6: 'vasc, Vascular skin lesion'
+  
+  };
+  /**
+ * @license
+ * Copyright 2017 Google LLC. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =============================================================================
+ */
+
+TARGET_CLASSES = {
+    0: 'akiec, Actinic Keratoses (Solar Keratoses) or intraepithelial Carcinoma (Bowen’s disease)',
+    1: 'bcc, Basal Cell Carcinoma',
+    2: 'bkl, Benign Keratosis',
+    3: 'df, Dermatofibroma',
+    4: 'mel, Melanoma',
+    5: 'nv, Melanocytic Nevi',
+    6: 'vasc, Vascular skin lesion'
+  
+  };
+  // Code is based on a YouTube tutorial by deeplizard
+// https://www.youtube.com/watch?v=HEQDRWMK6yY
+
+
+
+// After the model loads we want to make a prediction on the default image.
+// Thus, the user will see predictions when the page is first loaded.
+
+function simulateClick(tabID) {
+	
+	document.getElementById(tabID).click();
+}
+
+
+$("#image-selector").change(function () {
+	let reader = new FileReader();
+	reader.onload = function () {
+		let dataURL = reader.result;
+		$("#selected-image").attr("src", dataURL);
+		$("#prediction-list").empty();
+	}
+	
+		
+		let file = $("#image-selector").prop('files')[0];
+		reader.readAsDataURL(file);
+		
+		
+		// Simulate a click on the predict button
+		// This introduces a 0.5 second delay before the click.
+		// Without this long delay the model loads but may not automatically
+		// predict.
+		setTimeout(simulateClick.bind(null,'predict-button'), 500);
+
+});
+
+
+
+
+let model;
+(async function () {
+	
+	model = await tf.loadModel('http://skin.test.woza.work/model_kaggle_version12/model.json');
+	$("#selected-image").attr("src", "http://skin.test.woza.work/assets/samplepic.jpg")
+	
+	
+	// Simulate a click on the predict button
+	predictOnLoad();
+	// Hide the model loading spinner
+	$('.progress-bar').hide();
+	
+	
+})();
+
+function predictOnLoad() {
+	
+	// Simulate a click on the predict button
+	setTimeout(simulateClick.bind(null,'predict-button'), 1000);
+};
+
+
+
+
+$("#predict-button").click(async function () {
+	
+	
+	
+	let image = $('#selected-image').get(0);
+	
+	// Pre-process the image
+	let tensor = tf.fromPixels(image)
+	.resizeNearestNeighbor([224,224])
+	.toFloat();
+	
+	
+	let offset = tf.scalar(127.5);
+	
+	tensor = tensor.sub(offset)
+	.div(offset)
+	.expandDims();
+	
+	
+	
+	
+	// Pass the tensor to the model and call predict on it.
+	// Predict returns a tensor.
+	// data() loads the values of the output tensor and returns
+	// a promise of a typed array when the computation is complete.
+	// Notice the await and async keywords are used together.
+	let predictions = await model.predict(tensor).data();
+	let top5 = Array.from(predictions)
+		.map(function (p, i) { // this is Array.map
+			return {
+				probability: p,
+				className: SKIN_CLASSES[i] // we are selecting the value from the obj
+			};
+				
+			
+		}).sort(function (a, b) {
+			return b.probability - a.probability;
+				
+		}).slice(0, 3);
+	
+	
+$("#prediction-list").empty();
+top5.forEach(function (p) {
+
+	$("#prediction-list").append(`<li>${p.className}: ${p.probability.toFixed(6)}</li>`);
+
+	
+	});
+	
+	
+});
+
+
+// Code is based on a YouTube tutorial by deeplizard
+// https://www.youtube.com/watch?v=HEQDRWMK6yY
+
+
+
+// After the model loads we want to make a prediction on the default image.
+// Thus, the user will see predictions when the page is first loaded.
+
+function simulateClick(tabID) {
+	
+	document.getElementById(tabID).click();
+}
+
+function predictOnLoad() {
+	
+	// Simulate a click on the predict button
+	setTimeout(simulateClick.bind(null,'predict-button'), 500);
+};
+
+
+$("#image-selector").change(function () {
+	let reader = new FileReader();
+	reader.onload = function () {
+		let dataURL = reader.result;
+		$("#selected-image").attr("src", dataURL);
+		$("#prediction-list").empty();
+	}
+	
+		
+		let file = $("#image-selector").prop('files')[0];
+		reader.readAsDataURL(file);
+		
+		
+		// Simulate a click on the predict button
+		// This introduces a 0.5 second delay before the click.
+		// Without this long delay the model loads but may not automatically
+		// predict.
+		setTimeout(simulateClick.bind(null,'predict-button'), 500);
+
+});
+
+
+
+
+let modelA;
+(async function () {
+	
+	model = await tf.loadModel('http://concept.test.woza.work/final_model_kaggle_version1/model.json');
+	$("#selected-image").attr("src", "http://concept.test.woza.work/assets/samplepic.jpg")
+	
+	
+	
+	// Hide the model loading spinner
+	$('.progress-bar').hide();
+	
+	// Simulate a click on the predict button
+	predictOnLoad();
+	
+})();
+
+
+
+
+
+
+$("#predict-button").click(async function () {
+	
+	
+	
+	let image = $('#selected-image').get(0);
+	
+	// Pre-process the image
+	let tensor = tf.fromPixels(image)
+	.resizeNearestNeighbor([224,224])
+	.toFloat();
+	
+	
+	let offset = tf.scalar(127.5);
+	
+	tensor = tensor.sub(offset)
+	.div(offset)
+	.expandDims();
+	
+	
+	
+	
+	// Pass the tensor to the model and call predict on it.
+	// Predict returns a tensor.
+	// data() loads the values of the output tensor and returns
+	// a promise of a typed array when the computation is complete.
+	// Notice the await and async keywords are used together.
+	let predictions = await model.predict(tensor).data();
+	let top5 = Array.from(predictions)
+		.map(function (p, i) { // this is Array.map
+			return {
+				probability: p,
+				className: SKIN_CLASSES[i] // we are selecting the value from the obj
+			};
+				
+			
+		}).sort(function (a, b) {
+			return b.probability - a.probability;
+				
+		}).slice(0, 7);
+	
+	
+$("#prediction-list").empty();
+top5.forEach(function (p) {
+
+	$("#prediction-list").append(`<li>${p.className}: ${p.probability.toFixed(6)}</li>`);
+
+	
+	});
+	
+	
+
+
+//#############################################################
+
+// ### 1. LOAD THE MODEL IMMEDIATELY WHEN THE PAGE LOADS
+
+//#############################################################
+
+
+// Define 2 helper functions
+
+function simulateClick(tabID) {
+	
+	document.getElementById(tabID).click();
+}
+
+
+
+function predictOnLoad() {
+	
+	// Simulate a click on the predict button
+	setTimeout(simulateClick.bind(null,'predict-button'), 500);
+}
+
+
+
+
+
+
+// LOAD THE MODEL
+
+let model;
+(async function () {
+	
+	model = await tf.loadModel('http://skin.test.woza.work/final_model_kaggle_version1/model.json');
+	$("#selected-image").attr("src", "http://skin.test.woza.work/assets/samplepic.jpg");
+	
+	// Hide the model loading spinner
+	// This line of html gets hidden:
+	// <div class="progress-bar">Ai is Loading...</div>
+	$('.progress-bar').hide();
+	
+	
+	// Simulate a click on the predict button.
+	// Make a prediction on the default front page image.
+	predictOnLoad();
+	
+	
+	
+})();
+
+
+
+	
+
+//######################################################################
+
+// ### 2. MAKE A PREDICTION ON THE FRONT PAGE IMAGE WHEN THE PAGE LOADS
+
+//######################################################################
+
+
+
+// The model images have size 96x96
+
+// This code is triggered when the predict button is clicked i.e.
+// we simulate a click on the predict button.
+$("#predict-button").click(async function () {
+	
+	let image = undefined;
+	
+	image = $('#selected-image').get(0);
+	
+	// Pre-process the image
+	let tensor = tf.fromPixels(image)
+	.resizeNearestNeighbor([224,224])
+	.toFloat();
+	
+	
+	let offset = tf.scalar(127.5);
+	
+	tensor = tensor.sub(offset)
+	.div(offset)
+	.expandDims();
+	
+	
+	// Pass the tensor to the model and call predict on it.
+	// Predict returns a tensor.
+	// data() loads the values of the output tensor and returns
+	// a promise of a typed array when the computation is complete.
+	// Notice the await and async keywords are used together.
+	
+	// TARGET_CLASSES is defined in the target_clssses.js file.
+	// There's no need to load this file because it was imported in index.html
+	let predictions = await model.predict(tensor).data();
+	let top5 = Array.from(predictions)
+		.map(function (p, i) { // this is Array.map
+			return {
+				probability: p,
+				className: TARGET_CLASSES[i] 
+			};
+				
+			
+		}).sort(function (a, b) {
+			return b.probability - a.probability;
+				
+		}).slice(0, 3);
+	
+
+		// Append the file name to the prediction list
+		var file_name = 'samplepic.jpg';
+		$("#prediction-list").append(`<li class="w3-text-blue fname-font" style="list-style-type:none;">${file_name}</li>`);
+		
+		//$("#prediction-list").empty();
+		top5.forEach(function (p) {
+		
+			// ist-style-type:none removes the numbers.
+			// https://www.w3schools.com/html/html_lists.asp
+			$("#prediction-list").append(`<li style="list-style-type:none;">${p.className}: ${p.probability.toFixed(3)}</li>`);
+		
+			
+		});
+	
+	
+});
+
+
+
+//######################################################################
+
+// ### 3. READ THE IMAGES THAT THE USER SELECTS
+
+// Then direct the code execution to app_batch_prediction_code.js
+
+//######################################################################
+
+
+
+
+// This listens for a change. It fires when the user submits images.
+
+$("#image-selector").change(async function () {
+	
+	// the FileReader reads one image at a time
+	fileList = $("#image-selector").prop('files');
+	
+	//$("#prediction-list").empty();
+	
+	// Start predicting
+	// This function is in the app_batch_prediction_code.js file.
+	model_processArray(fileList);
+	
+});
+
+
+
+
+
+
+
+//################################################################################
+
+// ### 1. MAKE A PREDICTION ON THE IMAGE OR MULTIPLE IMAGES THAT THE USER SUBMITS
+
+//#################################################################################
+
+
+
+
+
+// the model images have size 96x96
+
+async function model_makePrediction(fname) {
+	
+	//console.log('met_cancer');
+	
+	// clear the previous variable from memory.
+	let image = undefined;
+	
+	image = $('#selected-image').get(0);
+	
+	// Pre-process the image
+	let tensor = tf.fromPixels(image)
+	.resizeNearestNeighbor([224,224])
+	.toFloat();
+	
+	
+	let offset = tf.scalar(127.5);
+	
+	tensor = tensor.sub(offset)
+	.div(offset)
+	.expandDims();
+
+	
+	// Pass the tensor to the model and call predict on it.
+	// Predict returns a tensor.
+	// data() loads the values of the output tensor and returns
+	// a promise of a typed array when the computation is complete.
+	// Notice the await and async keywords are used together.
+	let predictions = await model.predict(tensor).data();
+	let top5 = Array.from(predictions)
+		.map(function (p, i) { // this is Array.map
+			return {
+				probability: p,
+				className: TARGET_CLASSES[i] // we are selecting the value from the obj
+			};
+				
+			
+		}).sort(function (a, b) {
+			return b.probability - a.probability;
+				
+		}).slice(0, 3);
+		
+	// Append the file name to the prediction list
+	$("#prediction-list").append(`<li class="w3-text-blue fname-font" style="list-style-type:none;">
+	${fname}</li>`);
+	
+	//$("#prediction-list").empty();
+	top5.forEach(function (p) {
+	
+		$("#prediction-list").append(`<li style="list-style-type:none;">${p.className}: ${p.probability.toFixed(3)}</li>`);
+	
+		
+	});
+	
+	// Add a space after the prediction for each image
+	$("#prediction-list").append(`<br>`);
+		
+}
+
+
+
+
+// =====================
+// The following functions help to solve the problems relating to delays 
+// in assigning the src attribute and the delay in model prediction.
+// Without this the model will produce unstable predictions because
+// it will not be predicting on the correct images.
+
+
+// This tutorial explains how to use async, await and promises to manage delays.
+// Tutorial: https://blog.lavrton.com/javascript-loops-how-to-handle-async-await-6252dd3c795
+// =====================
+
+
+
+function model_delay() {
+	
+	return new Promise(resolve => setTimeout(resolve, 200));
+}
+
+
+async function model_delayedLog(item, dataURL) {
+	
+	// We can await a function that returns a promise.
+	// This delays the predictions from appearing.
+	// Here it does not actually serve a purpose.
+	// It's here to show how a delay like this can be implemented.
+	await model_delay();
+	
+	// display the user submitted image on the page by changing the src attribute.
+	// The problem is here. Too slow.
+	$("#selected-image").attr("src", dataURL);
+	$("#displayed-image").attr("src", dataURL); //#########
+	
+	// log the item only after a delay.
+	//console.log(item);
+}
+
+// This step by step tutorial explains how to use FileReader.
+// Tutorial: http://tutorials.jenkov.com/html5/file-api.html
+
+async function model_processArray(array) {
+	
+	for(var item of fileList) {
+		
+		
+		let reader = new FileReader();
+		
+		// clear the previous variable from memory.
+		let file = undefined;
+	
+		
+		reader.onload = async function () {
+			
+			let dataURL = reader.result;
+			
+			await model_delayedLog(item, dataURL);
+			
+			
+			
+			var fname = file.name;
+			
+			// clear the previous predictions
+			$("#prediction-list").empty();
+			
+			// 'await' is very important here.
+			await model_makePrediction(fname);
+		}
+		
+		file = item;
+		
+		// Print the name of the file to the console
+        //console.log("i: " + " - " + file.name);
+			
+		reader.readAsDataURL(file);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
